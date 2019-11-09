296354d19135b576cfdceee69c4255ecb6be0450
falifalifali <55840600+falifalifali@users.noreply.github.com>
2019-11-09 18:02:51 +0400

    4

---
+++ instagram-api-settings.php
@@ -0,0 +1,9 @@
+<?php
+
+define('INSTAGRAM_CLIENT_ID', '427392571534590');
+
+define('INSTAGRAM_CLIENT_SECRET', '7d3e5444f72a9f49c98ddc5f5dcd8d4d');
+
+define('INSTAGRAM_REDIRECT_URI', 'https://falifalifali.github.io/mytest/');
+
+?>
